9446e5864f5ae424edd4b141a0480f554e5b26b5
freddywijaya <vald.karate@gmail.com>
2016-09-17 21:35:11 +0700

    fixing

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -2,7 +2,7 @@ var express= require('express'),
 path=require('path');
 utils = require(path.join(__dirname, "../utils.js"));
 var routes = function(User,jwt,app){
-  var AuthRoutes = express.Router();
+  var AuthRouter = express.Router();
   var UserController=require('../Controllers/UserController')(User,jwt,app);
   // for api routes
   AuthRouter.route('/').post(UserController.auth);
